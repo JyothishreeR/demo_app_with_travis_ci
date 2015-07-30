031342c04bce33d937858922b428f153f5d1da08
Shaiju E <eshaiju@gmail.com>
2015-07-30 11:32:03 +0530

    removed unwanted migration file

--- db/migrate/20150721061724_remove_fields_from_users.rb
+++
@@ -1,9 +0,0 @@
-class RemoveFieldsFromUsers < ActiveRecord::Migration
-  def change
-  	remove_column :users, :image_file_name
-  	remove_column :users, :image_content_type
-  	remove_column :users, :image_file_size
-  	remove_column :users, :image_updated_at
-  end
-end
- 
